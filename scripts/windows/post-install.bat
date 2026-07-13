d1a0fe1207b561fb49d85490680a6ca0488d62d0
Christophe Roger <croger42@gmail.com>
2026-07-13 19:10:25 +0200

    update post-install

--- scripts/windows/post-install.bat
+++ scripts/windows/post-install.bat
@@ -2,11 +2,14 @@
 set PROJECT_DIR=%~dp0..\..\
 pushd %PROJECT_DIR%
 set PROJECT_DIR=%CD%
+set TARGET_DIR=%PROJECT_DIR%\target
 popd
-set EXE=%PROJECT_DIR%\target\fluppy.exe
+copy 
+set EXE=%PROJECT_DIR%\target\fluppybox.exe
+copy %PROJECT_DIR%\target\fluppy.exe %EXE%
 where rcedit >nul 2>&1 || scoop install rcedit
 rcedit "%EXE%"  --set-icon "%PROJECT_DIR%\src\main\resources\dosdog2.ico"
 :: remove console mode
 powershell -ExecutionPolicy Bypass -Command "$bytes=[IO.File]::ReadAllBytes('%EXE%'); $pe=[BitConverter]::ToInt32($bytes,0x3C); $bytes[$pe+0x5C]=2; [IO.File]::WriteAllBytes('%EXE%',$bytes)"
-cp %EXE% E:/
+copy %EXE% E:/
 
